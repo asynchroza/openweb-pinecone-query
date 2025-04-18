8281712cc156070f7904385f169890df45921eeb
Michael Bozhilov <michaelbozhilov@gmail.com>
2025-04-18 15:15:40 +0300

    Emit found files

--- script.py
+++ script.py
@@ -5,36 +5,36 @@ author_url: https://asynchroza.com
 git_url: https://github.com/asynchroza/openweb-pinecone-query
 description: Queries data in Pinecone by embedding queries using small sentence models
 required_open_webui_version: 0.6.0
-requirements: pinecone
+requirements: pinecone, sentence_transformers, pydantic, aiohttp
 version: 0.1.0
 licence: MIT
 """
 
 from sentence_transformers import SentenceTransformer
 from pydantic import BaseModel, Field
+from typing import Optional, Callable, Awaitable, Any
 import pinecone
+import aiohttp
 
 
 class Tools:
     def __init__(self):
         self.valves = self.Valves()
 
-
     class Valves(BaseModel):
         pinecone_api_key: str = Field("", description="Your Pinecone API")
         pinecone_index: str = Field("", description="Pinecone Index Name")
-        sentence_transformer_model: str = Field("all-MiniLM-L6-v2", description="Sentence Tranformer Model")
+        sentence_transformer_model: str = Field(
+            "all-MiniLM-L6-v2", description="Sentence Tranformer Model"
+        )
 
-    def search_pinecone_index(
-            self,
-            query: str
-        ) -> str:
+    async def search(
+        self,
+        query: str,
+        __event_emitter__: Optional[Callable[[Any], Awaitable[None]]] = None,
+    ) -> str:
         """
-        This function retrieves relevant information from a Pinecone index to assist with code-related questions.
-
-        Whenever the LLM receives a query about code—such as how to write a specific function, what a piece of code does, or any other programming-related inquiry—it should call this function with the user's query.
-
-        The function will search the Pinecone index for code snippets and documentation that can help answer the question, explain code behavior, or provide useful context for developers.
+        Search and find the answer to any question in the knowledgebase.
 
         :param query: The user's code-related question or search string.
         """
@@ -51,13 +51,26 @@ class Tools:
 
         formatted = []
         for match in results["matches"]:
-            file_name = match['metadata'].get('file') 
-            code = match['metadata'].get('code') 
+            score = match.get("score")
+            file_name = match["metadata"].get("file")
+            code = match["metadata"].get("code")
+
+            if __event_emitter__:
+                await __event_emitter__(
+                    {
+                        "type": "message",
+                        "data": {
+                            "content": f"Found a match '{file_name}' with score of {score}\n"
+                        },
+                    }
+                )
+
             formatted.append(f"Filename: {file_name} -- Code: {code}")
 
         return "\n\n---\n\n".join(formatted)
 
+
 if __name__ == "__main__":
     tools = Tools()
-    print(tools.search_pinecone_index("How to add item to python list"))
+    print(tools.search("How to add item to python list"))
 
